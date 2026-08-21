cf7676ae86610a886b4fcd80a20f43da8633afc3
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-21 20:06:32 +0800

    Make FastScanner seekable and robust for large lines

--- include/FastScanner.h
+++ include/FastScanner.h
@@ -10,13 +10,18 @@ public:
     ~FastScanner();
 
     bool valid() const;
+    bool seek(uint64_t offset, bool skipPartialLine);
     bool nextLine(const char*& data, size_t& length, uint64_t& offset);
 
 private:
+    bool ensureLineCapacity(size_t required);
+
     void* fp_;
     char* buffer_;
+    char* line_;
     size_t bufferSize_;
+    size_t lineCapacity_;
     size_t pos_;
     size_t size_;
-    uint64_t offset_;
+    uint64_t absoluteOffset_;
 };
