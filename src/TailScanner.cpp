b81027d1c037d1df44ac479d393b83a7abbf86a3
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-21 10:46:55 +0800

    Improve TailScanner with reverse RANGEA search

--- src/TailScanner.cpp
+++ src/TailScanner.cpp
@@ -3,31 +3,57 @@
 #include <vector>
 #include <cstring>
 
-static bool findFirstRange(const char* data, size_t size, GPST& t)
+static bool findLastRange(const char* data, size_t size, GPST& t)
 {
-    for(size_t i=0;i+7<size;i++)
+    if(size < 7)
+        return false;
+
+    for(size_t i = size - 7; ; --i)
     {
-        if(data[i]=='#' && std::strncmp(data+i,"#RANGEA",7)==0)
+        if(data[i] == '#' &&
+           std::strncmp(data + i, "#RANGEA", 7) == 0)
         {
-            return parseRangeTime(data+i,t);
+            if(parseRangeTime(data + i, t))
+                return true;
         }
+
+        if(i == 0)
+            break;
     }
+
     return false;
 }
 
 bool detectTailRange(const char* filename, FileTimeRange& range)
 {
-    std::ifstream fin(filename,std::ios::binary|std::ios::ate);
+    std::ifstream fin(filename, std::ios::binary | std::ios::ate);
     if(!fin)
         return false;
 
-    std::streamoff size=fin.tellg();
-    const size_t block=64*1024*1024;
+    std::streamoff size = fin.tellg();
 
-    std::vector<char> buffer((size_t)(size<block?size:block));
+    const size_t blocks[] =
+    {
+        64ULL * 1024 * 1024,
+        256ULL * 1024 * 1024,
+        1024ULL * 1024 * 1024
+    };
 
-    fin.seekg(size-(std::streamoff)buffer.size());
-    fin.read(buffer.data(),buffer.size());
+    for(size_t k = 0; k < 3; k++)
+    {
+        size_t readSize = blocks[k];
+        if((std::streamoff)readSize > size)
+            readSize = (size_t)size;
 
-    return findFirstRange(buffer.data(),buffer.size(),range.end);
+        std::vector<char> buffer(readSize);
+
+        fin.clear();
+        fin.seekg(size - (std::streamoff)readSize);
+        fin.read(buffer.data(), buffer.size());
+
+        if(findLastRange(buffer.data(), buffer.size(), range.end))
+            return true;
+    }
+
+    return false;
 }
