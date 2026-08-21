f1c0208e3a2851acd65269124ebf8e5cf90a78b5
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-21 10:48:41 +0800

    Integrate GPST offset estimation and seek based extraction

--- src/FastExtractByGPST.cpp
+++ src/FastExtractByGPST.cpp
@@ -1,20 +1,31 @@
 #include "FastExtractByGPST.h"
+#include "FileTimeDetector.h"
+#include "GpstLocator.h"
 #include "RangeParser.h"
+
 #include <fstream>
 #include <string>
 
-static bool inside(const GPST& t,const GPST& s,const GPST& e)
+static bool inside(const GPST& t, const GPST& s, const GPST& e)
 {
-    if(t.week<s.week || t.week>e.week)
+    if (t.week < s.week || t.week > e.week)
         return false;
 
-    if(t.week==s.week && t.sow<s.sow)
+    if (t.week == s.week && t.sow < s.sow)
         return false;
 
-    if(t.week==e.week && t.sow>e.sow)
+    if (t.week == e.week && t.sow > e.sow)
         return false;
 
     return true;
+}
+
+static bool afterEnd(const GPST& t, const GPST& e)
+{
+    if (t.week > e.week)
+        return true;
+
+    return (t.week == e.week && t.sow > e.sow);
 }
 
 int fastExtractByGPST(
@@ -23,37 +34,56 @@ int fastExtractByGPST(
     const GPST& start,
     const GPST& end)
 {
-    std::ifstream fin(input,std::ios::binary);
-    std::ofstream fout(output,std::ios::binary);
+    FileTimeRange range;
+    if (!detectFileTimeRange(input, range) || !range.valid)
+        return -2;
 
-    if(!fin || !fout)
-        return -1;
+    FileTimeInfo info;
+    info.start = range.start;
+    info.end = range.end;
+    info.valid = true;
+
+    std::ifstream fin(input, std::ios::binary | std::ios::ate);
+    if (!fin)
+        return -3;
+
+    uint64_t fileSize = static_cast<uint64_t>(fin.tellg());
+
+    uint64_t offset = 0;
+    if (!estimateOffset(fileSize, info, start, offset))
+        return -4;
+
+    fin.seekg(static_cast<std::streamoff>(offset));
+
+    std::ofstream fout(output, std::ios::binary);
+    if (!fout)
+        return -5;
 
     std::string line;
-    bool writing=false;
+    bool writing = false;
 
-    while(std::getline(fin,line))
+    while (std::getline(fin, line))
     {
         GPST t;
 
-        if(parseRangeTime(line.c_str(),t))
+        if (parseRangeTime(line.c_str(), t))
         {
-            if(inside(t,start,end))
-                writing=true;
-            else if(writing)
+            if (inside(t, start, end))
             {
-                if(t.week>end.week ||
-                  (t.week==end.week && t.sow>end.sow))
-                    break;
+                writing = true;
+            }
+            else if (writing && afterEnd(t, end))
+            {
+                break;
             }
         }
 
-        if(writing)
+        if (writing)
         {
-            fout.write(line.data(),(std::streamsize)line.size());
+            fout.write(line.data(), static_cast<std::streamsize>(line.size()));
             fout.put('\n');
         }
     }
 
-    return 0;
+    return writing ? 0 : -6;
 }
