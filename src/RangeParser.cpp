29b5c79059b66f84aae736585205e745e3a90ece
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-21 10:51:06 +0800

    Implement zero-copy RANGEA time parser

--- src/RangeParser.cpp
+++ src/RangeParser.cpp
@@ -2,34 +2,79 @@
 #include <cstring>
 #include <cstdlib>
 
-bool parseRangeTime(const char* line, GPST& t)
+static bool parseIntField(const char* p, int& value)
 {
-    if (std::strncmp(line, "#RANGEA", 7) != 0)
-        return false;
+    value = 0;
 
-    int field = 0;
-    const char* p = line;
-
-    while (*p)
+    while (*p >= '0' && *p <= '9')
     {
-        if (*p == ',')
-        {
-            ++field;
-            ++p;
+        value = value * 10 + (*p - '0');
+        ++p;
+    }
 
-            if (field == 5)
-                t.week = std::atoi(p);
-            else if (field == 6)
-            {
-                t.sow = std::atof(p);
-                return true;
-            }
+    return true;
+}
+
+static bool parseDoubleField(const char* p, double& value)
+{
+    value = 0.0;
+
+    double scale = 0.1;
+    bool decimal = false;
+
+    while (*p >= '0' && *p <= '9')
+    {
+        if (decimal)
+        {
+            value += (*p - '0') * scale;
+            scale *= 0.1;
         }
         else
         {
+            value = value * 10.0 + (*p - '0');
+        }
+
+        ++p;
+
+        if (*p == '.')
+        {
+            decimal = true;
             ++p;
         }
     }
 
+    return true;
+}
+
+bool parseRangeTimeFast(const char* data, size_t length, GPST& t)
+{
+    if (length < 7 || std::memcmp(data, "#RANGEA", 7) != 0)
+        return false;
+
+    int field = 0;
+
+    for (size_t i = 0; i < length; ++i)
+    {
+        if (data[i] != ',')
+            continue;
+
+        ++field;
+
+        if (field == 5)
+        {
+            parseIntField(data + i + 1, t.week);
+        }
+        else if (field == 6)
+        {
+            parseDoubleField(data + i + 1, t.sow);
+            return true;
+        }
+    }
+
     return false;
+}
+
+bool parseRangeTime(const char* line, GPST& t)
+{
+    return parseRangeTimeFast(line, std::strlen(line), t);
 }
