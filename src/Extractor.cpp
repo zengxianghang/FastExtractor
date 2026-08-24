bc26d39c344d2f1c49648f1cb5b93768f981169e
xianghang zeng <62739219+zengxianghang@users.noreply.github.com>
2026-08-24 16:38:32 +0800

    Select NAV mode explicitly

--- src/Extractor.cpp
+++ src/Extractor.cpp
@@ -3,7 +3,11 @@
 int extractRange(const char* input,
                  const char* output,
                  const GPST& start,
-                 const GPST& end)
+                 const GPST& end,
+                 bool keepNavigation)
 {
-    return fastExtractByGPST(input, output, start, end);
+    if (keepNavigation)
+        return fastExtractByGPST(input, output, start, end);
+
+    return fastExtractByGPSTNoNav(input, output, start, end);
 }
